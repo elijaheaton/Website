392adc88323f24667c676a75a6fdcddcec4046c5
Elijah Eaton <eaton.elijah@gmail.com>
2022-10-07 20:00:10 -0500

    title looks good on mobile, need colors nicer

--- js/website.js
+++ js/website.js
@@ -1,4 +1,10 @@
+let media = window.matchMedia("(max-width: 776px)");
+size(media);
+media.addEventListener('resize', size);
+
 window.onscroll = function() {scrollFunction()};
+
+
 
 function scrollFunction() {
     let name = document.getElementById("name");
@@ -7,14 +13,24 @@ function scrollFunction() {
     
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         name.style.width = "0";
-        //name.style.fontSize = "80%";
-        name.style.fontsize = "2rem";
+        name.style.fontSize = smaller;
         console.log(name.style.fontsize);
         banner.style.marginTop = header.offsetHeight.toString() + "px";
     } else {
         name.style.width = "100%";
-        //name.style.fontSize = "100%";
-        name.style.fontsize = "4rem";
+        name.style.fontSize = larger;
         banner.style.marginTop = header.offsetHeight.toString() + "px";
     }
 }
+
+function size(media) {
+    if (media.matches) {
+        smaller = "1.5rem";
+        larger = "3rem";
+    }
+    else {
+        smaller = "2rem";
+        larger = "4rem";
+    }
+}
+
